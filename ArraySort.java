627143a9d8554dd7f3078218ff8fafaf37d4a79e
BRavinder <ravindervasu5@gmail.com>
2023-07-23 15:51:49 +0530

    changes

---
+++ ArraySort.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArraySort {
+    public static void main(String[] args) {
+        int a1[] = new int[]{3,6,2,9,5,8,1};
+        for(int i = 0; i < a1.length; i++){
+            System.out.print(a1[i]);
+        }
+        Arrays.sort(a1);
+        System.out.println();
+        for(int i = 0; i < a1.length; i++){
+            System.out.print(a1[i]);
+        }
+    }
+}
